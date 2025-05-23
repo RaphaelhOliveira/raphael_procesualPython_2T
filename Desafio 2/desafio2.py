dc92284bab9feeb655321850976fdce02004775c
RaphaelhOliveira <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-05-23 11:15:20 -0300

    Create desafio2.py

---
+++ Desafio 2/desafio2.py
@@ -0,0 +1,9 @@
+aniversario = 23
+controlhe = 0
+
+while controlhe <=100:
+    multiplicacao = controlhe * aniversario
+    controlhe +=1
+    if multiplicacao > 1000:
+        print (f"O numero {multiplicacao} é maior que 1000")
+    print (multiplicacao)
