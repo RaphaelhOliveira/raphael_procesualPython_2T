258bcfaf51d367eda2a6d1c9d24731a2aa65b9c9
RaphaelhOliveira <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-05-28 10:06:18 -0300

    Create desafio4.py

---
+++ Desafio 4/desafio4.py
@@ -0,0 +1,54 @@
+for i in range(1,101):
+    if i == 2:
+        print(f"primo:\n {i}")
+    if i == 3:
+        print(f"primo: {i}")
+    if i == 5:
+        print(f"primo: {i}")    
+    if i == 7:
+        print(f"primo: {i}")
+    if i == 11:
+        print(f"primo: {i}")
+    if i == 13:
+        print(f"primo: {i}")
+    if i == 17:
+        print(f"primo: {i}")
+    if i == 19:
+        print(f"primo: {i}")
+    if i == 23:
+        print(f"primo: {i}")
+    if i == 29:
+        print(f"primo: {i}")
+    if i == 31:
+        print(f"primo: {i}")
+    if i == 37:
+        print(f"primo: {i}")
+    if i == 41:
+        print(f"primo: {i}")
+    if i == 43:
+        print(f"primo: {i}")
+    if i == 47:
+        print(f"primo: {i}")
+    if i == 53:
+        print(f"primo: {i}")
+    if i == 59:
+        print(f"primo: {i}")
+    if i == 61:
+        print(f"primo: {i}")
+    if i == 67:
+        print(f"primo: {i}")
+    if i == 71:
+        print(f"primo: {i}")
+    if i == 73:
+        print(f"primo: {i}")
+    if i == 79:
+        print(f"primo: {i}")
+    if i == 83:
+        print(f"primo: {i}")
+    if i == 89:
+        print(f"primo: {i}")
+    if i == 97:
+        print(f"primo: {i}")
+    if i % 2 == 0:
+        print(f"divisível por 2: {i}")
+    print(i)
